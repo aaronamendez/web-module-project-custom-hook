7c14c03b26f5f73419e611a889994cb459415d7f
Ant Man <34754995+aaronamendez@users.noreply.github.com>
2022-01-12 16:56:46 -0500

    Cleaned up the code a bit an added comments.

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,17 +1,23 @@
 import { useState } from 'react';
 
-export const useLocalStorage = (key, initialValue) => {
-	const [value, setValue] = useState(() => {
+export const useLocalStorage = (key, valuePair) => {
+	// Checks Local Storage for a key and it's value pair to see
+	// if a value already exists, if not, add one.
+	const checkLocalStorage = () => {
 		if (localStorage.getItem(key)) {
-			return JSON.parse(localStorage.getItem(key));
+			return localStorage.getItem(key);
 		} else {
-			localStorage.setItem(key, JSON.stringify(initialValue));
-			return initialValue;
+			localStorage.setItem(key, valuePair);
+			return valuePair;
 		}
-	});
+	};
+
+	// Here we create a value and invoke useState,
+	// while passing in our checkLocalStorage expression
+	const [value, setValue] = useState(checkLocalStorage);
 
 	const setStoredValue = (newValue) => {
-		localStorage.setItem(key, JSON.stringify(newValue));
+		localStorage.setItem(key, newValue);
 		setValue(newValue);
 	};
 
